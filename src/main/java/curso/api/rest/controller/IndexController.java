d9af1e0093990c74c06ba5a9bd0d71a281cbabdd
thbd <thiagobalbino91@gmail.com>
2020-01-16 18:54:34 -0300

    retornando usuario em json para a tela

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -1,5 +1,8 @@
 package curso.api.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,21 +10,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import curso.api.rest.model.Usuario;
+
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
-
-//	@GetMapping(value = "/", produces = "application/json")
-//	public ResponseEntity init() {
-//		return new ResponseEntity("Olá Usuário REST Spring Boot", HttpStatus.OK);
-//	}
 	
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init2(@RequestParam (value = "nome", defaultValue = "nome nao informado", required = true) String nome, 
-								@RequestParam (value = "salario", defaultValue = "salario nao informado", required = true) String salario) {		
+	public ResponseEntity<Usuario> init() {		
+		
+		Usuario usuario = new Usuario();
+		usuario.setId(39L);
+		usuario.setLogin("login@login");
+		usuario.setSenha("123");
+		usuario.setNome("thiago");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setId(23L);
+		usuario2.setLogin("maria@maria");
+		usuario2.setSenha("454654");
+		usuario2.setNome("maria");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(usuario);
+		usuarios.add(usuario2);
+		
 		//para testar:
-		//http://localhost:8080/usuario/?nome=thiago&salario=102
-		return new ResponseEntity("Olá Usuário REST Spring Boot. Nome: " + nome + " => salario: " + salario, HttpStatus.OK);
+		//http://localhost:8080/usuario/?nome=thiago&salario=102		
+		return new ResponseEntity(usuarios, HttpStatus.OK);
 	}
 	
 }
