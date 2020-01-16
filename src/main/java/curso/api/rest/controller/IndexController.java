23dc56febb41838ea50d7f419180848b641606ac
thbd <thiagobalbino91@gmail.com>
2020-01-16 19:13:45 -0300

    criando serviceo restful de consulta de usuario

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -2,42 +2,45 @@ package curso.api.rest.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
-	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> init() {		
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {		
 		
-		Usuario usuario = new Usuario();
-		usuario.setId(39L);
-		usuario.setLogin("login@login");
-		usuario.setSenha("123");
-		usuario.setNome("thiago");
-		
-		Usuario usuario2 = new Usuario();
-		usuario2.setId(23L);
-		usuario2.setLogin("maria@maria");
-		usuario2.setSenha("454654");
-		usuario2.setNome("maria");
-		
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		usuarios.add(usuario);
-		usuarios.add(usuario2);
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
 		
 		//para testar:
-		//http://localhost:8080/usuario/?nome=thiago&salario=102		
-		return new ResponseEntity(usuarios, HttpStatus.OK);
+		//http://localhost:8080/usuario/5
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> init2() {		
+		
+		List<Usuario> usuarios = (List<Usuario>) usuarioRepository.findAll();
+		
+		//para testar:
+		//http://localhost:8080/usuario/
+		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
 	}
 	
 }
