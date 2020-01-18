a689df2ef989f55919cc50f3dc99a5ee0fc30e59
thbd <thiagobalbino91@gmail.com>
2020-01-18 10:18:36 -0300

    Json Web Toke (JWT) - Cadastrando e editando

--- src/main/java/curso/api/rest/controller/IndexController.java
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,6 +47,8 @@ public class IndexController {
 			usuario.getTelefones().get(i).setUsuario(usuario);
 		}
 		
+		String senhacriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(senhacriptografada);
 		Usuario usuarioSalvo = usuarioRepository.save(usuario);
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
