0a6e79b13aff564d4d5bbe0eba1ea8acf2fd4db5
thbd <thiagobalbino91@gmail.com>
2020-01-18 09:03:40 -0300

    JSON Web Token (JWT) - Testando a autenticacao do JSON

--- src/main/java/curso/api/rest/CursospringrestapiApplication.java
+++ src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -22,6 +23,7 @@ public class CursospringrestapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringrestapiApplication.class, args);
+//		System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
