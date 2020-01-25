ef0a484e486cba302c4ce0fa7a4a58008ba7d967
thbd <thiagobalbino91@gmail.com>
2020-01-25 19:56:45 -0300

    aula angular - mod 36 - criando o controller advice parte 2 - tratamento erros

--- src/main/java/curso/api/rest/ControleExcecoes.java
+++ src/main/java/curso/api/rest/ControleExcecoes.java
@@ -1,6 +1,7 @@
 package curso.api.rest;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import org.hibernate.exception.ConstraintViolationException;
 import org.postgresql.util.PSQLException;
@@ -8,6 +9,8 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +26,19 @@ public class ControleExcecoes extends ResponseEntityExceptionHandler {
 	@ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
 	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
 			HttpStatus status, WebRequest request) {
+		
+		String msg = "";
+		
+		if (ex instanceof MethodArgumentNotValidException) {
+			List<ObjectError> listError= ((MethodArgumentNotValidException) ex).getBindingResult().getAllErrors();
+			
+			for (ObjectError objectError : listError) {
+				msg += objectError.getDefaultMessage() + "\n";
+			}						
+		}
+		else {
+			msg = ex.getMessage();
+		}
 		
 		ObjetoError objetoError = new ObjetoError();
 		objetoError.setError(ex.getMessage());
